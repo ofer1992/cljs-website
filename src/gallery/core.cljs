b72424e48d3f1acef2f2375b78ce48787b721772
Ofer Yehuda <ofer@sproutt.com>
2023-09-18 11:01:15 +0300

    first commit

---
+++ src/gallery/core.cljs
@@ -0,0 +1,34 @@
+(ns gallery.core
+  (:require
+    [reagent.dom :as rdom]
+    [reagent.core :as r]
+    [gallery.views.bin2dec :as bin2dec]
+    ))
+;; define a state atom that determines which project is displayed
+(defonce project (r/atom nil))
+
+;; map of projects availble
+(def projects {"bin2dec" bin2dec/main-panel})
+
+
+(defn project-component [component]
+  [:div 
+    [component]])
+
+(defn main-panel []
+  [:div 
+   [:h1 "Welcome to my portfolio"]
+   [:p "Here are my projects:"]
+;;    [:ul (for [[text component] projects] [:li text]) ]
+   ;; unordered list of available projects, click changes project to display
+   [:ul (for [[text component] projects] [:li {:on-click #(reset! project component)} text]) ]
+   [:hr]
+   ;; if project is not nil, display it
+    (when @project
+        [project-component @project]
+    )
+;;    
+   ])
+
+(defn ^:export main [] 
+  (rdom/render [main-panel] (js/document.getElementById "app")))
---
+++ src/gallery/views/bin2dec.cljs
@@ -0,0 +1,35 @@
+(ns gallery.views.bin2dec
+    ( :require 
+        [reagent.core :as r]
+        [clojure.set :as set]
+    ))
+
+(defn atom-input [value]
+  [:input {:type "text"
+           :value @value
+           :on-change #(reset! value (-> % .-target .-value))}])
+
+(defn valid-binary? [bin]
+    (set/subset? (set bin) (set "01"))
+)
+
+(defn bin2dec [bin]
+    (reduce #(+ (js/parseInt %2) (* %1 2)) bin)
+)
+
+(defn project []
+  (let [bin (r/atom "0")]
+  (fn [] 
+  [:div
+    [:h1 "bin2dec"]
+    [:p "Binary: " [atom-input bin] ]
+    (if (valid-binary? @bin)
+        (bin2dec @bin)
+        [:p "Bad binary rep"])
+  ]
+  )
+  )
+)
+
+(defn main-panel []
+ [project])
